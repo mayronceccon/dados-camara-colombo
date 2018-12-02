a78d9f5a1bbddbd2c0bd128895f611fbc71de488
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2018-12-02 12:43:42 -0200

    Ajustar configuração da pasta static

--- dadoscolombo/settings.py
+++ dadoscolombo/settings.py
@@ -120,11 +120,11 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
