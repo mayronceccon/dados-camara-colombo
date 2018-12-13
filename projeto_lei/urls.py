405bf5d7be2ed30f5ae76ea161913ffc2e84b7df
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2018-12-12 23:22:41 -0200

    Inserir url para consultar projetos

--- projeto_lei/urls.py
+++ projeto_lei/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
+    path('', views.index, name='index'),
     path('salvar', views.salvar, name='salvar'),
     path('extrair_info', views.extrair_info, name='extrair_info'),
 ]
--- projeto_lei/views.py
+++ projeto_lei/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.core.cache import cache
 from .models import ProjetoLei
+
+
+def index(request):
+    cache_key = 'projetos_list'
+    # 23 horas
+    cache_time = (60 * 60 * 23)
+    data = cache.get(cache_key)
+
+    if not data:
+        jsonresponse = []
+        projetos = ProjetoLei.objects.all().order_by('-projeto')
+        for projeto in projetos:
+            jsonresponse.append({
+                "projeto": projeto.projeto,
+                "protocolo": projeto.protocolo,
+                "assunto": projeto.assunto,
+                "vereador": {
+                    "nome": projeto.vereador.nome,
+                    "apelino": projeto.vereador.apelido
+                },
+                "data_divulgacao": projeto.data_divulgacao,
+                "data_aprovacao": projeto.data_aprovacao,
+                "data_arquivamento": projeto.data_arquivamento
+            })
+        data = jsonresponse
+        cache.set(cache_key, data, cache_time)
+
+    return JsonResponse({"results": data})
 
 
 def salvar(request):
