14dfc08c57bca6bcc57d5b563bc94717ee421752
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2018-12-15 08:19:24 -0200

    Ajustes de código

--- vereador/admin.py
+++ vereador/admin.py
@@ -5,6 +5,6 @@ from .models import Vereador
 class VereadorAdmin(admin.ModelAdmin):
     ordering = ('-nome',)
     fields = ('nome', 'apelido', 'data_nascimento', 'email', 'telefone_gabinete', 'observacao', 'foto', 'legislaturas', 'ativo')
-    list_display = ['nome', 'apelido', 'legislaturas']
+    list_display = ['nome', 'apelido']
 
 admin.site.register(Vereador, VereadorAdmin)
--- vereador/views.py
+++ vereador/views.py
@@ -9,7 +9,7 @@ def index(request):
     cache_time = (60 * 60 * 23)
     data = cache.get(cache_key)
 
-    if True:
+    if not data:
         jsonresponse = []
         vereadores = Vereador.objects.all().order_by('nome')
         for vereador in vereadores:
