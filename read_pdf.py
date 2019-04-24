8ae8898781d4af2101719f62917b087db362c5fe
Mayron Ceccon <mayron.ceccon@tecnorisk.com.br>
2019-04-24 16:19:54 -0300

    Interpretar pautas em pdf gerando dados trabalhaveis

---
+++ read_pdf.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+from tika import parser
+import re
+
+def indicacao_numero(indicacao):
+    regex = r"((?:N°:\s)([0-9]{1,})(?:\sAutor))"
+    matches = re.search(regex, indicacao)
+    if matches is not None:
+        return matches.group(2)
+
+def indicacao_autor(indicacao):
+    regex = r"((?:Autor:\s|Autora:\s)(.*)(?:\sDestinatário))"
+    matches = re.search(regex, indicacao)
+    if matches is not None:
+        return matches.group(2)
+
+def indicacao_destinatario(indicacao):
+    regex = r"((?:Destinatário:\s)(.*)(?:\sAssunto:))"
+    matches = re.search(regex, indicacao)
+    if matches is not None:
+        return matches.group(2)
+
+def indicacao_assunto(indicacao):
+    regex = r"((?:Assunto:\s)(.*))"
+    matches = re.search(regex, indicacao)
+    if matches is not None:
+        return matches.group(2)
+
+
+arquivo = 'http://www.camaracolombo.pr.gov.br/pauta/2018/sessao_06_03_2018.pdf'
+arquivo = 'sessao_06_03_2018.pdf'
+arquivo = 'http://www.camaracolombo.pr.gov.br/pauta/2018/sessao_20_03_2018.pdf'
+
+raw = parser.from_file(arquivo)
+content = raw['content']
+content = content.strip().rstrip('\r\n').replace("\n", "").replace("\r", "").replace("  ", " ")
+content = content.strip().rstrip('\r\n').replace("\n", "").replace("\r", "").replace("  ", " ")
+
+# print(content)
+# exit()
+
+regex = r"(?:cação\s)(.*?)(?:\sIndi|\sColombo, [0-9]{1,2} de)"
+matches = re.findall(regex, content)
+if matches is not None:
+    for match in matches:
+        numero = indicacao_numero(match)
+        autor = indicacao_autor(match)
+        destinatario = indicacao_destinatario(match)
+        assunto = indicacao_assunto(match)
+        text = "%s\n%s\n%s\n%s\n" % (numero, autor, destinatario, assunto)
+        print(text)
+
