2e26495aaf5e12be165d0c33b916e75b6c26d9f5
Mayron Ceccon <mayron.ceccon@tecnorisk.com.br>
2019-04-24 16:36:14 -0300

    Inserir atualização do pip no deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 git pull
+/home/dados-colombo/dados-camara-colombo/venv/bin/pip install --upgrade pip
 /home/dados-colombo/dados-camara-colombo/venv/bin/pip install -r requirements.txt
 /home/dados-colombo/dados-camara-colombo/venv/bin/python manage.py collectstatic --no-input
 /home/dados-colombo/dados-camara-colombo/venv/bin/python manage.py makemigrations --no-input
