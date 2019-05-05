b24d49cfe9f79582237b1697110837cc141c3f0a
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2019-05-05 12:39:22 -0300

    Inserir opção de filtros na busca de indicações

--- indicacao/views.py
+++ indicacao/views.py
@@ -1,10 +1,12 @@
-import json
 import datetime
+import json
 
 from django.core import serializers
 from django.core.cache import cache
+from django.db.models import Q
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+
 from rest_framework import viewsets
 
 from .models import Indicacao
@@ -13,9 +15,43 @@ from .services import IndicacaoServices
 
 
 class IndicacaoViewSet(viewsets.ModelViewSet):
-    queryset = Indicacao.objects.all().order_by('-pauta__data_sessao', '-numero')
+    queryset = Indicacao.objects.all().order_by(
+        '-pauta__data_sessao',
+        '-numero'
+    )
     serializer_class = IndicacaoSerializer
     http_method_names = ['get']
+
+    def get_queryset(self):
+        vereador = self.request.query_params.get('vereador', None)
+        if vereador is not None:
+            find = (
+                Q(vereador__nome__icontains=vereador) | 
+                Q(vereador__apelido__icontains=vereador)
+            )
+            if vereador.isnumeric():
+                find = Q(vereador=vereador)
+            self.queryset = self.queryset.filter(find)
+
+        pauta = self.request.query_params.get('pauta', None)
+        if pauta is not None:
+            find = Q(pauta__descricao__icontains=pauta)
+            if pauta.isnumeric():
+                find = Q(pauta=pauta)
+            self.queryset = self.queryset.filter(find)
+
+        destinatario = self.request.query_params.get('destinatario', None)
+        if destinatario is not None:
+            find = Q(destinatario__nome__icontains=destinatario)
+            if destinatario.isnumeric():
+                find = Q(destinatario=destinatario)
+            self.queryset = self.queryset.filter(find)
+
+        assunto = self.request.query_params.get('assunto', None)
+        if assunto is not None:
+            find = Q(assunto__icontains=assunto)
+            self.queryset = self.queryset.filter(find)
+        return self.queryset
 
 
 def index(request):
