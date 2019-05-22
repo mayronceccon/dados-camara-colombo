538c762ee1dd40267aacceaca929dcbfe8e1ce98
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2019-05-22 19:05:18 -0300

    Ajustes configurações do projeto

--- dadoscolombo/settings.py
+++ dadoscolombo/settings.py
@@ -147,6 +147,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 25
 }
+
+APPEND_SLASH = False
