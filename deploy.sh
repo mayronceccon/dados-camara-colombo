fb29b58446b927bb8b867a99d84cc9a1241cfc00
Dados Colombo <dados-colombo@vps8670.publiccloud.com.br>
2019-12-02 16:54:40 -0300

    Update file deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git pull
-/home/dados-colombo/dados-camara-colombo/venv/bin/pip install --upgrade pip
-/home/dados-colombo/dados-camara-colombo/venv/bin/pip install -r requirements.txt
-/home/dados-colombo/dados-camara-colombo/venv/bin/python manage.py collectstatic --no-input
-/home/dados-colombo/dados-camara-colombo/venv/bin/python manage.py makemigrations --no-input
-/home/dados-colombo/dados-camara-colombo/venv/bin/python manage.py migrate --no-input
+/home/dados-colombo/dados-camara-colombo/venv/bin/pip3.7 install --upgrade pip
+/home/dados-colombo/dados-camara-colombo/venv/bin/pip3.7 install -r requirements.txt
+/home/dados-colombo/dados-camara-colombo/venv/bin/python3.7 manage.py collectstatic --no-input
+/home/dados-colombo/dados-camara-colombo/venv/bin/python3.7 manage.py makemigrations --no-input
+/home/dados-colombo/dados-camara-colombo/venv/bin/python3.7 manage.py migrate --no-input
 supervisorctl restart dados_colombo
