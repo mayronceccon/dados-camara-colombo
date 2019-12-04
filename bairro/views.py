cb64a68f6398341745db7a8b6f0247d6b4918fcb
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2019-12-04 20:42:10 -0200

    Remove librarioes not utilized

--- bairro/views.py
+++ bairro/views.py
@@ -1,7 +1,5 @@
 from rest_framework import viewsets
 from rest_framework import mixins
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
 
 from .models import Bairro
 from .serializers import BairroSerializer
