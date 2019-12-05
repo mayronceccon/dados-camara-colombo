1a81ec28baa9f4a04f804050f95a0ce2b9018a4c
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2019-12-05 21:48:41 -0200

    Add cache in pages of councilmen

--- vereador/views.py
+++ vereador/views.py
@@ -1,8 +1,8 @@
 from django.core.cache import cache
 from django.http import HttpResponse, JsonResponse
-
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework import viewsets
-
 from .models import Vereador
 from .serializers import VereadorSerializer
 
@@ -11,3 +11,11 @@ class VereadorViewSet(viewsets.ModelViewSet):
     queryset = Vereador.objects.all().order_by('nome')
     serializer_class = VereadorSerializer
     http_method_names = ['get']
+
+    @method_decorator(cache_page(60*60*23))
+    def retrieve(self, request, pk=None):
+        return super().retrieve(request, pk)
+
+    @method_decorator(cache_page(60*60*23))
+    def list(self, request):
+        return super().list(request)
