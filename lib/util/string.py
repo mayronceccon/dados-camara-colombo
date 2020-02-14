9a38be3f00aeee5cda39a0adb46dc98ccf20c665
Mayron Ceccon <mayron.ceccon@gmail.com>
2020-02-14 13:33:52 -0300

    Adicionar no sanitize a limpeza pelo normalize

--- lib/util/string.py
+++ lib/util/string.py
@@ -2,6 +2,14 @@ import re
 import unicodedata
 
 
+def normalize(conteudo):
+    conteudo = conteudo.strip() \
+        .rstrip('\r\n') \
+        .replace("\n", "") \
+        .replace("\r", "")
+    return re.sub('[ ]{2,}', ' ', conteudo)
+
+
 def sanitize(palavra):
     # Unicode normalize transforma um caracter em seu equivalente em latin.
     nfkd = unicodedata.normalize('NFKD', palavra)
@@ -9,12 +17,4 @@ def sanitize(palavra):
 
     # Usa expressão regular para retornar
     # a palavra apenas com números, letras e espaço
-    return re.sub('[^a-zA-Z0-9 \\\]', ' ', palavra)
-
-
-def normalize(conteudo):
-    conteudo = conteudo.strip() \
-        .rstrip('\r\n') \
-        .replace("\n", "") \
-        .replace("\r", "")
-    return re.sub('[ ]{2,}', ' ', conteudo)
+    return normalize(re.sub('[^a-zA-Z0-9 \\\]', ' ', palavra))
