61f9497374140f9ed12b08fef3e0a93a5edb2b30
Mayron Ceccon <mayron.ceccon@gmail.com>
2020-02-18 14:00:23 -0300

    Inserir busca de número da indicação para pautas a partir de 2020

--- indicacao/busca_dados/numero.py
+++ indicacao/busca_dados/numero.py
@@ -4,7 +4,7 @@ from indicacao.busca_dados.busca import Busca
 
 class Numero(Busca):
     def recuperar(self, dados):
-        regex = r"((?:N°[:]?[\s]?)([0-9]{1,})((?:\s{0,}Autor|Autora)))"
+        regex = r"((?:N[°|º][:]?[\s]?)([0-9]{1,})((?:\s{0,}Autor|\s{0,}Autora|\s{0,}Ano [0-9]{4})))"
         matches = re.search(regex, dados.lstrip())
         if matches is not None:
             return int(matches.group(2))
--- indicacao/test/busca_dados/test_numero.py
+++ indicacao/test/busca_dados/test_numero.py
@@ -3,9 +3,12 @@ from indicacao.busca_dados.numero import Numero
 
 
 class NumeroTest(TestCase):
-    def setUp(self):
-        self.__dados = '  N°:222   Autor:Renato Lunardon   Destinatário: Secretaria de Urbanismo   Assunto: Venho solicitar ao setor competente a   viabilidade de construçãode uma travessia   elevada na Rua Vereador Angelim Walesko frente ao número 351,   paralelas às Ruas Angelina Cavalli e Luiz Gasparin,   no bairro Jardim Eucaliptos, neste município.    '
-
-    def test_recuperar_numero(self):
-        numero = Numero().recuperar(self.__dados)
+    def test_recuperar_numero_anterior_2020(self):
+        dados = '  N°:222   Autor:Renato Lunardon   Destinatário: Secretaria de Urbanismo   Assunto: Venho solicitar ao setor competente a   viabilidade de construçãode uma travessia   elevada na Rua Vereador Angelim Walesko frente ao número 351,   paralelas às Ruas Angelina Cavalli e Luiz Gasparin,   no bairro Jardim Eucaliptos, neste município.    '
+        numero = Numero().recuperar(dados)
         self.assertEqual(222, numero)
+
+    def test_recuperar_numero_ano_2020(self):
+        dados = ' Nº 1 Ano 2020 Autor: Anderson Ferreira da Silva Destinatário: Secretaria Municipal de Saúde Assunto: Estudos para promover doações de bicicleta com tração compartilhada - motorizada e humana, para agentes comunitários de saúde.'
+        numero = Numero().recuperar(dados)
+        self.assertEqual(1, numero)
