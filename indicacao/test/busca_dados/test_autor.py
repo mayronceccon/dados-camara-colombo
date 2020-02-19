dbca642790c9310aca84c792cbe65fe62e54b788
Mayron Ceccon <mayron.ceccon@gmail.com>
2020-02-19 19:01:22 -0300

    Inserir test de busca de autor das pautas a partir de 2020

--- indicacao/test/busca_dados/test_autor.py
+++ indicacao/test/busca_dados/test_autor.py
@@ -3,9 +3,12 @@ from indicacao.busca_dados.autor import Autor
 
 
 class AutorTest(TestCase):
-    def setUp(self):
-        self.__dados = '  N°:222   Autor:Renato Lunardon   Destinatário: Secretaria de Urbanismo   Assunto: Venho solicitar ao setor competente a   viabilidade de construçãode uma travessia   elevada na Rua Vereador Angelim Walesko frente ao número 351,   paralelas às Ruas Angelina Cavalli e Luiz Gasparin,   no bairro Jardim Eucaliptos, neste município.    '
-
-    def test_identificar_autor(self):
-        autor = Autor().recuperar(self.__dados)
+    def test_identificar_autor_anterior_2020(self):
+        dados = '  N°:222   Autor:Renato Lunardon   Destinatário: Secretaria de Urbanismo   Assunto: Venho solicitar ao setor competente a   viabilidade de construçãode uma travessia   elevada na Rua Vereador Angelim Walesko frente ao número 351,   paralelas às Ruas Angelina Cavalli e Luiz Gasparin,   no bairro Jardim Eucaliptos, neste município.    '
+        autor = Autor().recuperar(dados)
         self.assertEqual('Renato Lunardon', autor)
+
+    def test_identificar_autor_2020(self):
+        dados = """Nº 1 Ano 2020 Autor: Anderson Ferreira da Silva Destinatário: Secretaria Municipal de Saúde Assunto: Estudos para promover doações de bicicleta com tração compartilhada - motorizada e humana, para agentes comunitários de saúde."""
+        autor = Autor().recuperar(dados)
+        self.assertEqual('Anderson Ferreira da Silva', autor)
