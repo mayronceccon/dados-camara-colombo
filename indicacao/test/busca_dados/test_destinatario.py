c53a01abcc1bd43b3953610f0120c9813a9f0c3c
Mayron Ceccon <mayron.ceccon@gmail.com>
2020-02-19 18:59:17 -0300

    Inserir busca de destinatários das pautas a partir de 2020

--- indicacao/test/busca_dados/test_destinatario.py
+++ indicacao/test/busca_dados/test_destinatario.py
@@ -3,9 +3,13 @@ from indicacao.busca_dados.destinatario import Destinatario
 
 
 class DestinatarioTest(TestCase):
-    def setUp(self):
-        self.__dados = '  N°:222   Autor:Renato Lunardon   Destinatário: Secretaria de Urbanismo   Assunto: Venho solicitar ao setor competente a   viabilidade de construçãode uma travessia   elevada na Rua Vereador Angelim Walesko frente ao número 351,   paralelas às Ruas Angelina Cavalli e Luiz Gasparin,   no bairro Jardim Eucaliptos, neste município.    '
-
-    def test_identificar_autor(self):
-        destinatario = Destinatario().recuperar(self.__dados)
+    def test_identificar_autor_anterior_2020(self):
+        dados = '  N°:222   Autor:Renato Lunardon   Destinatário: Secretaria de Urbanismo   Assunto: Venho solicitar ao setor competente a   viabilidade de construçãode uma travessia   elevada na Rua Vereador Angelim Walesko frente ao número 351,   paralelas às Ruas Angelina Cavalli e Luiz Gasparin,   no bairro Jardim Eucaliptos, neste município.    '
+        destinatario = Destinatario().recuperar(dados)
         self.assertEqual('SECRETARIA DE URBANISMO', destinatario)
+
+    def test_identificar_autor_2020(self):
+        dados = """Nº 1 Ano 2020 Autor: Anderson Ferreira da Silva Destinatário: Secretaria Municipal de Saúde Assunto: Estudos para promover doações de bicicleta com tração compartilhada - motorizada e humana, para agentes comunitários de saúde."""
+
+        destinatario = Destinatario().recuperar(dados)
+        self.assertEqual('SECRETARIA MUNICIPAL DE SAUDE', destinatario)
