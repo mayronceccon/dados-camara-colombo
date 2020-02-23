c5383d45678fffc4f2e22383584c6c3a0c8066b0
Mayron Ceccon <mayron.ceccon@gmail.com>
2020-02-23 13:35:02 -0300

    Inserir filtros para busca de indicações

--- indicacao/views.py
+++ indicacao/views.py
@@ -2,6 +2,7 @@ import datetime
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
+from django.db.models import Q
 from rest_framework.decorators import action
 from rest_framework import viewsets
 from .models import Indicacao
@@ -10,12 +11,39 @@ from .services1 import IndicacaoPautaServices
 
 
 class IndicacaoViewSet(viewsets.ModelViewSet):
-    queryset = Indicacao.objects.all().order_by(
-        '-pauta__data_sessao',
-        '-numero'
-    )
     serializer_class = IndicacaoSerializer
     http_method_names = ['get']
+
+    def get_queryset(self):
+        queryset = Indicacao.objects.all().order_by(
+            '-pauta__data_sessao',
+            '-numero'
+        )
+        assunto = self.request.query_params.get('assunto', None)
+        if assunto is not None:
+            queryset = queryset.filter(
+                assunto__icontains=assunto
+            )
+
+        destinatario = self.request.query_params.get('destinatario', None)
+        if destinatario is not None:
+            queryset = queryset.filter(
+                destinatario__nome__icontains=destinatario
+            )
+
+        vereador = self.request.query_params.get('vereador', None)
+        if vereador is not None:
+            if vereador.isnumeric():
+                queryset = queryset.filter(
+                    vereador__id=vereador
+                )
+            else:
+                queryset = queryset.filter(
+                    Q(vereador__nome__icontains=vereador) |
+                    Q(vereador__apelido__icontains=vereador)
+                )
+
+        return queryset
 
     # @method_decorator(cache_page(60*60*23))
     def retrieve(self, request, pk=None):
