2af9e79eb13da573a7588b25df98649028df6b18
Mayron Ceccon <mayron.ceccon@gmail.com>
2020-05-24 17:51:32 -0300

    Inserir a busca da data da sessão juntamente com a busca do arquivo

--- pauta/services/link_pauta.py
+++ pauta/services/link_pauta.py
@@ -1,5 +1,6 @@
 import re
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 
 class LinkPauta:
@@ -9,22 +10,31 @@ class LinkPauta:
     def info(self):
         res = BeautifulSoup(self.__html, "html5lib")
 
-        titles = res.find_all(
+        list_pautas = res.find_all(
             "font",
             string=re.compile(r'[0-9]{2}\/[0-9]{2}\/[0-9]{4}')
         )
         sessoes = []
-        for title in titles:
-            t = title.text.strip()
-            href = title.find_parent("a")["href"]
-
-            regex = r"\?down=[0-9]{1,}"
-            matches = re.search(regex, href)
-            if matches is not None:
-                href = f"http://www.midias.camaracolombo.pr.gov.br/edital_sessoes.php{href}"
+        for pauta in list_pautas:
+            title = pauta.text.strip()
+            date = self.__get_date(title)
+            href = pauta.find_parent("a")["href"]
+            href = self.__href_process(href)
 
             sessoes.append({
-                "title": t,
-                "href": href
+                "title": title,
+                "href": href,
+                "date": date
             })
         return sessoes
+
+    def __href_process(self, href):
+        regex = r"\?down=[0-9]{1,}"
+        matches = re.search(regex, href)
+        if matches is not None:
+            href = f"http://www.midias.camaracolombo.pr.gov.br/edital_sessoes.php{href}"
+        return href
+
+    def __get_date(self, title):
+        d = title.split("-")[0].strip()
+        return datetime.strptime(d, '%d/%m/%Y')
--- pauta/test/services/test_link_pauta.py
+++ pauta/test/services/test_link_pauta.py
@@ -1,6 +1,7 @@
 import os
 from django.test import TestCase
 from pauta.services.link_pauta import LinkPauta
+import datetime
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -13,36 +14,81 @@ class LinkPautaTest(TestCase):
         data = LinkPauta(html)
 
         expected = [
-            {"title": "27/05/2020",
-                "href": "http://www.midias.camaracolombo.pr.gov.br/edital_sessoes.php?down=4"},
-            {"title": "19/05/2020",
-                "href": "http://www.midias.camaracolombo.pr.gov.br/edital_sessoes.php?down=3"},
-            {"title": "12/05/2020",
-                "href": "http://www.midias.camaracolombo.pr.gov.br/edital_sessoes.php?down=2"},
-            {"title": "05/05/2020",
-                "href": "http://www.midias.camaracolombo.pr.gov.br/edital_sessoes.php?down=1"},
-            {"title": "28/04/2020",
-                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_28_04_2020.pdf"},
-            {"title": "22/04/2020",
-                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_22_04_2020.pdf"},
-            {"title": "16/04/2020 - Extraordinária",
-                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_16_04_2020.pdf"},
-            {"title": "14/04/2020",
-                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_14_04_2020.pdf"},
-            {"title": "17/03/2020",
-                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_17_03_2020.pdf"},
-            {"title": "10/03/2020",
-                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_10_03_2020.pdf"},
-            {"title": "03/03/2020",
-                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_03_03_2020.pdf"},
-            {"title": "27/02/2020",
-                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_27_02_2020.pdf"},
-            {"title": "18/02/2020",
-                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_18_02_2020.pdf"},
-            {"title": "11/02/2020",
-                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_11_02_2020.pdf"},
-            {"title": "04/02/2020",
-                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_04_02_2020.pdf"},
+            {
+                "title": "27/05/2020",
+                "href": "http://www.midias.camaracolombo.pr.gov.br/edital_sessoes.php?down=4",
+                "date": datetime.datetime(2020, 5, 27, 0, 0)
+            },
+            {
+                "title": "19/05/2020",
+                "href": "http://www.midias.camaracolombo.pr.gov.br/edital_sessoes.php?down=3",
+                "date": datetime.datetime(2020, 5, 19, 0, 0)
+            },
+            {
+                "title": "12/05/2020",
+                "href": "http://www.midias.camaracolombo.pr.gov.br/edital_sessoes.php?down=2",
+                "date": datetime.datetime(2020, 5, 12, 0, 0)
+            },
+            {
+                "title": "05/05/2020",
+                "href": "http://www.midias.camaracolombo.pr.gov.br/edital_sessoes.php?down=1",
+                "date": datetime.datetime(2020, 5, 5, 0, 0)
+            },
+            {
+                "title": "28/04/2020",
+                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_28_04_2020.pdf",
+                "date": datetime.datetime(2020, 4, 28, 0, 0)
+            },
+            {
+                "title": "22/04/2020",
+                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_22_04_2020.pdf",
+                "date": datetime.datetime(2020, 4, 22, 0, 0)
+            },
+            {
+                "title": "16/04/2020 - Extraordinária",
+                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_16_04_2020.pdf",
+                "date": datetime.datetime(2020, 4, 16, 0, 0)
+            },
+            {
+                "title": "14/04/2020",
+                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_14_04_2020.pdf",
+                "date": datetime.datetime(2020, 4, 14, 0, 0)
+            },
+            {
+                "title": "17/03/2020",
+                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_17_03_2020.pdf",
+                "date": datetime.datetime(2020, 3, 17, 0, 0)
+            },
+            {
+                "title": "10/03/2020",
+                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_10_03_2020.pdf",
+                "date": datetime.datetime(2020, 3, 10, 0, 0)
+            },
+            {
+                "title": "03/03/2020",
+                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_03_03_2020.pdf",
+                "date": datetime.datetime(2020, 3, 3, 0, 0)
+            },
+            {
+                "title": "27/02/2020",
+                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_27_02_2020.pdf",
+                "date": datetime.datetime(2020, 2, 27, 0, 0)
+            },
+            {
+                "title": "18/02/2020",
+                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_18_02_2020.pdf",
+                "date": datetime.datetime(2020, 2, 18, 0, 0)
+            },
+            {
+                "title": "11/02/2020",
+                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_11_02_2020.pdf",
+                "date": datetime.datetime(2020, 2, 11, 0, 0)
+            },
+            {
+                "title": "04/02/2020",
+                "href": "http://www.camaracolombo.pr.gov.br/pauta/2020/sessao_04_02_2020.pdf",
+                "date": datetime.datetime(2020, 2, 4, 0, 0)
+            },
         ]
 
         self.assertCountEqual(
