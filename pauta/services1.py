1b09237fb94b467b57665d6a5f6b26ea959c0024
Mayron Ceccon <mayron.ceccon@gmail.com>
2020-05-24 17:52:02 -0300

    Refatorar a classe que busca as pautas e utilizar a nova classe de buscas da pautas

--- pauta/services1.py
+++ pauta/services1.py
@@ -1,10 +1,5 @@
 from django.db import IntegrityError
-from bs4 import BeautifulSoup
 from urllib.request import urlopen
-from datetime import datetime
-from lib.notificacao import send
-import re
-
 from .models import Pauta
 from .services.link_pauta import LinkPauta
 
@@ -15,75 +10,27 @@ class PautaServices:
 
     def salvar_busca(self):
         dados = self.busca_arquivos_sessao()
-        notificar = False
         for dado in dados:
             try:
-                descricao = dado['titulo']
                 pauta = Pauta(
-                    descricao=descricao,
-                    link=dado['arquivo'],
-                    data_sessao=dado['data']
+                    descricao=dado['title'],
+                    link=dado['href'],
+                    data_sessao=dado['date']
                 )
                 pauta.save()
-                notificar = True
             except IntegrityError:
-                error = "Registro ja existente - %s" % dado['titulo']
+                error = "Registro ja existente - %s" % dado['title']
                 print(error)
 
-        if notificar:
-            send.enviar(
-                "Nova Pauta disponível - %s" % (descricao),
-                'https://camaracolombo.com.br/pautas'
-            )
-
     def __urls(self):
-        url_pauta = "http://www.midias.camaracolombo.pr.gov.br/edital_sessoes.php"
-        return {
-            'pauta': url_pauta
-        }
+        return "http://www.midias.camaracolombo.pr.gov.br/edital_sessoes.php"
 
     def busca_arquivos_sessao(self):
-        sessoes = self.__dados_html()
-        dados = []
-        for sessao in sessoes:
-            compl = None
-            titulo = None
-            href = sessao['href']
-            arquivo_sessao = self.__arquivo_sessao(sessao)
-
-            # busca nomes arquivos
-            regex = r"(/sessao_)(.*)(.pdf)"
-            matches = re.search(regex, href)
-            data = matches.group(2).replace('_', '-')
-
-            # busca datas
-            regex2 = r"(\d{2}-\d{2}-\d{4})-(\d{0,})"
-            matches = re.search(regex2, data)
-            if matches is not None:
-                data = matches.group(1)
-                compl = 'Extraordinária'
-
-            # titulo
-            data = datetime.strptime(data, '%d-%m-%Y')
-            titulo = data.strftime("%d/%m/%Y")
-            if compl is not None:
-                titulo = "%s - %s" % (titulo, compl)
-
-            titulo = "Sessão - %s" % titulo
-
-            dados.append({
-                'titulo': titulo,
-                'data': data,
-                'arquivo': arquivo_sessao
-            })
-        return dados
+        return self.__dados_html()
 
     def __dados_html(self):
-        url_pauta = self.__urls()['pauta']
+        url_pauta = self.__urls()
 
         html = urlopen(url_pauta)
         data = LinkPauta(html.read())
         return data.info()
-
-    def __arquivo_sessao(self, sessao):
-        return sessao['href']
